f4ad9e4de6888edb52f5c3ff1cde0bb9f96fee82
JACKSON POULOSE <141388076+jacksonpoulose@users.noreply.github.com>
2025-03-10 20:07:47 +0000

    before hosting

--- config/passport.js
+++ config/passport.js
@@ -9,8 +9,8 @@ passport.deserializeUser(function(user, done) {
 }); 
 
 passport.use(new GoogleStrategy({ 
-	clientID:process.env.GOOGLE_CLIENT_ID, // Your Credentials here. 
-	clientSecret:process.env.GOOGLE_CLIENT_SECRET, // Your Credentials here. 
+	clientID:process.env.GOOGLE_CLIENT_ID, 
+	clientSecret:process.env.GOOGLE_CLIENT_SECRET, 
 	callbackURL:"http://localhost:3000/auth/google/callback", 
 	passReqToCallback:true
 }, 
